4ca45a8fbc492f79a122fc9133f6c8eea10666e5
Lisitha Dissanayaka <114801465+LisithaDiz@users.noreply.github.com>
2024-07-23 10:10:26 +0530

    Update Lab_4_q3.scala

--- lab_sub_4/Lab_4_q3.scala
+++ lab_sub_4/Lab_4_q3.scala
@@ -1,36 +1,23 @@
 object StringFormatter {
-  def toUpper(input: String): String = {
-    input.toUpperCase()
-  }
+  def toUpper(input: String): String = input.toUpperCase()
 
-  def toLower(input: String): String = {
-    input.toLowerCase()
-  }
+  def toLower(input: String): String = input.toLowerCase()
 
-  def formatNames(name: String)(formatFunction: String => String): String = {
-    formatFunction(name)
-  }
+  def formatNames(name: String)(formatFunction: String => String): String = formatFunction(name)
 
   def main(args: Array[String]): Unit = {
     val names = List("Benny", "Niroshan", "Saman", "Kumara")
 
-    val formattedNames = names.map {
-      case "Benny" => formatNames("Benny")(toUpper)
-      case "Niroshan" => formatNames("Niroshan")(name => {
-        val upper = name.substring(0, 2).toUpperCase()
-        val lower = name.substring(2).toLowerCase()
-        upper + lower
-      })
-      case "Saman" => formatNames("Saman")(toLower)
-      case "Kumara" => formatNames("Kumara")(name => {
-        val upper = name.substring(0, 1).toUpperCase()
-        val lower = name.substring(1, 5).toLowerCase()
-        val upperEnd = name.substring(5).toUpperCase()
-        upper + lower + upperEnd
-      })
-      case other => other 
-    }
-
-    formattedNames.foreach(println)
+    // Test cases
+    println(formatNames(names(0))(toUpper))     
+    println(formatNames(names(1)) { name =>
+      val formattedName = name.substring(0, 2).toUpperCase + name.substring(2).toLowerCase
+      formattedName
+    })                                         
+    println(formatNames(names(2))(toLower))       
+    println(formatNames(names(3)) { name =>
+      val formattedName = name.substring(0, name.length - 1).toLowerCase + name.last.toUpper
+      formattedName
+    })                                          
   }
 }
