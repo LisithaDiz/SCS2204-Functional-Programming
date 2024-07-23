6768eb21a01c6bb132b188e496a9081420cbfecb
Lisitha Dissanayaka <114801465+LisithaDiz@users.noreply.github.com>
2024-07-23 09:16:48 +0530

    Update Lab_4_q2.scala

--- lab_sub_4/Lab_4_q2.scala
+++ lab_sub_4/Lab_4_q2.scala
@@ -1,21 +1,15 @@
-object PatternMatchingExample {
+object PatternMatching {
   def main(args: Array[String]): Unit = {
-    if (args.length != 1) {
-      println("Please provide exactly one integer input.")
+    if (args.length == 0) {
+      println("Please provide an integer input.")
       return
     }
 
-    val input = try {
-      args(0).toInt
-    } catch {
-      case _: NumberFormatException =>
-        println("Invalid input. Please provide a valid integer.")
-        return
-    }
+    val input = args(0).toInt
 
     input match {
-      case n if n <= 0 => println("Negative/Zero is input")
-      case n if n % 2 == 0 => println("Even number is given")
+      case x if x <= 0 => println("Negative/Zero is input")
+      case x if x % 2 == 0 => println("Even number is given")
       case _ => println("Odd number is given")
     }
   }
